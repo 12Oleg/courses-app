e9e58a52a984bf578d36e1f761fed8c1fd5c2fce
Oleg <seleznev0708@gmail.com>
2023-05-06 23:02:25 +0300

    fix test1

--- src/components/Courses/components/CourseCard/tests/courseCard.test.js
+++ src/components/Courses/components/CourseCard/tests/courseCard.test.js
@@ -32,15 +32,17 @@ describe('CourseCard', () => {
 		</Provider>
 	);
 
-	it('should display title', () => {
+	beforeEach(() => {
 		render(provider);
+	});
+
+	it('should display title', () => {
 		expect(
 			screen.getByText('Course title', { selector: 'h1.h1' })
 		).toBeInTheDocument();
 	});
 
 	it('should display description', () => {
-		render(provider);
 		expect(
 			screen.getByText('Course description', {
 				selector: 'div.courseCard__first-part div',
@@ -49,7 +51,6 @@ describe('CourseCard', () => {
 	});
 
 	it('should display duration in the correct format', () => {
-		render(provider);
 		expect(
 			screen.getByText('02:10 hours', {
 				selector: 'div.courseCard__second-part div',
@@ -58,7 +59,6 @@ describe('CourseCard', () => {
 	});
 
 	it('should display authors list', () => {
-		render(provider);
 		expect(
 			screen.getByText('John Doe, Tim Cob', {
 				selector: 'div.courseCard__second-part div',
@@ -67,7 +67,6 @@ describe('CourseCard', () => {
 	});
 
 	it('should display created date in the correct format', () => {
-		render(provider);
 		const dateRegex = /^(?:[1-9]|[12]\d|3[01])\/(?:[1-9]|1[0-2])\/\d{4}$/;
 		const createdDate = screen.getByText(dateRegex, {
 			selector: 'div.courseCard__second-part div',
